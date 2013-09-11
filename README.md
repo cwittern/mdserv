c3ccb0446a56248d4ab1ae4fe03389a7b6dea277
Christian Wittern <chris@dao.zinbun.kyoto-u.ac.jp>
2013-09-11 13:41:08 +0900

    latest version on dao

--- README.md
+++
@@ -1,4 +0,0 @@
-mdserv
-======
-
-Server part of the mandoku client / server version
--- local_settings.py
+++ local_settings.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-#DATABASE='/tmp/kanripo.db'
-DEBUG=True
-SECRET_KEY='~[ \xab\xff\xf2\x08\xc8\x9aG\x1d\xd2\xd7a\xde9\xac;\xa1\xb4A\xaf\xbe\x15'
-USERNAME='admin'
-PASSWORD='default'
-
 #mandoku
 MDBASE="/Users/Shared/md-remote"
 TXTDIR="/Users/Shared/md-remote/text"
